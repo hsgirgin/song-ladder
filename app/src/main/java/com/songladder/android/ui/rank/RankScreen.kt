77fb78217564938f069dc558627ef0b0eb62adc7
hsgirgin <hsgirgin@gmail.com>
2026-08-18 23:37:41 +0200

    Fix RankScreen semantics import

--- app/src/main/java/com/songladder/android/ui/rank/RankScreen.kt
+++ app/src/main/java/com/songladder/android/ui/rank/RankScreen.kt
@@ -48,6 +48,7 @@ import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.CustomAccessibilityAction
 import androidx.compose.ui.semantics.customActions
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
