1f4ae78bd61f94450953efedbee529f77b38a140
hsgirgin <hsgirgin@gmail.com>
2026-08-19 12:44:00 +0200

    Fix mutable swipe state delegate

--- app/src/main/java/com/songladder/android/ui/rank/RankScreen.kt
+++ app/src/main/java/com/songladder/android/ui/rank/RankScreen.kt
@@ -39,6 +39,7 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
