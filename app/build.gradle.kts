545d67cea7e90f91608645d943119e33aa96398b
hsgirgin <hsgirgin@gmail.com>
2026-08-25 23:18:30 +0200

    Bump version to 1.7 (versionCode 8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.songladder.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.6"
+        versionCode = 8
+        versionName = "1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
