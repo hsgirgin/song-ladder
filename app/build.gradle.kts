d667955facc4f042f4f59b0bd5ec3498ffd0fde1
hsgirgin <hsgirgin@gmail.com>
2026-08-27 12:18:41 +0200

    Bump version to 1.8 (versionCode 9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.songladder.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
