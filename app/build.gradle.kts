11217425a1a4ee6056349f14981663c466620db1
hsgirgin <hsgirgin@gmail.com>
2026-08-28 14:30:14 +0200

    Bump version to 1.11 (versionCode 12)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.songladder.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.10"
+        versionCode = 12
+        versionName = "1.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
