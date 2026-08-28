8d627daae947f01bf0df42febc0b5092c407f836
hsgirgin <hsgirgin@gmail.com>
2026-08-28 16:18:55 +0200

    Bump version to 1.14 (versionCode 15)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.songladder.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.13"
+        versionCode = 15
+        versionName = "1.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
