cd35868b9b01bd042ec3c30ecf7c32e3c9ea2622
hsgirgin <hsgirgin@gmail.com>
2026-08-29 20:10:42 +0200

    Bump version to 1.21 (versionCode 22)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.songladder.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "1.20"
+        versionCode = 22
+        versionName = "1.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
