1c3feac169b3df1a28a82cd2804a9605ccf4d50b
Hüseyin Seyfi Girgin <hsgirgin@gmail.com>
2026-08-29 22:00:54 +0000

    Bump version to 1.23 (versionCode 24)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.songladder.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "1.22"
+        versionCode = 24
+        versionName = "1.23"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
