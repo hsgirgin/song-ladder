e93e0f08fd15029229464020f83b396531f6582d
hsgirgin <hsgirgin@gmail.com>
2026-09-01 03:16:56 +0200

    Bump version to 1.25 (versionCode 26)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.songladder.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "1.24"
+        versionCode = 26
+        versionName = "1.25"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
